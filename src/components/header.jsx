77c14cd5d7f5670e791fee7034fbc1fe6715a42a
iturres <emanueliturres.co@gmail.com>
2023-07-24 19:35:57 -0300

    feat: On 'Header.jsx' added new Bootstrap classes and -span- element on 'my-profile' a-link

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -5,27 +5,44 @@ import icon from '../images/planet.png';
 import '../styles/header.css';
 
 const MainHeader = () => (
-  <Navbar bg="light" variant="light" expand="md">
+  <Navbar bg="light" variant="light" expand="md" className="p-4">
     <Container>
       <NavLink to="/" style={{ cursor: 'pointer', textDecoration: 'none' }}>
-        <figure className="d-flex align-items-center gap-2">
+        <figure className="d-flex align-items-center gap-2 m-0">
           <Image src={icon} width="40px" />
-          <h1>Space Traveller</h1>
+          <h1>SpaceX Traveler&apos;s Hub</h1>
         </figure>
       </NavLink>
 
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
       <Navbar.Collapse id="responsive-navbar-nav">
         <Nav className="ms-auto">
-          <NavLink to="/" style={{ cursor: 'pointer' }} className="nav-link fs-5" activeClassName="active">
+          <NavLink
+            to="/"
+            style={{ cursor: 'pointer' }}
+            className="nav-link fs-5 pb-1"
+            activeclassname="active"
+          >
             Rockets
           </NavLink>
-          <NavLink to="/missions" style={{ cursor: 'pointer' }} className="nav-link fs-5" activeClassName="active">
+          <NavLink
+            to="/missions"
+            style={{ cursor: 'pointer' }}
+            className="nav-link fs-5 pb-1"
+            activeclassname="active"
+          >
             Missions
           </NavLink>
-          <NavLink to="/profile" style={{ cursor: 'pointer' }} className="nav-link fs-5" activeClassName="active">
-            My Profile
-          </NavLink>
+          <span className="my-profile-link">
+            <NavLink
+              to="/profile"
+              style={{ cursor: 'pointer' }}
+              className="nav-link fs-5"
+              activeclassname="active"
+            >
+              My Profile
+            </NavLink>
+          </span>
         </Nav>
       </Navbar.Collapse>
     </Container>
