af3c1fae966ad0700e2511eb65367e441b9ce27e
iturres <emanueliturres.co@gmail.com>
2023-07-25 15:58:15 -0300

    feat: Added 'missionsReducer' to 'store' reducers

--- src/redux/config-store.js
+++ src/redux/config-store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import rocketsReducer from './rockets/rockets-slice';
+import missionsReducer from './missions/missions-slice';
 
 const store = configureStore({
   reducer: {
     rockets: rocketsReducer,
+    missions: missionsReducer,
   },
 });
 
