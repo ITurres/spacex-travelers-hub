4a45b13cbd8ee302b13fc96e97f293ba6e31c600
iturres <emanueliturres.co@gmail.com>
2023-07-25 17:49:08 -0300

    feat: on 'MissionsPage' > select all data from Missions state to render it

--- src/pages/MissionsPage.jsx
+++ src/pages/MissionsPage.jsx
@@ -1,11 +1,13 @@
 import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { fetchMissions } from '../redux/missions/missions-slice';
+import MissionsList from '../components/missions/MissionsList';
 
 let readyToFetch = true;
 
 const MissionsPage = () => {
   const dispatch = useDispatch();
+  const { missions, isLoading, error } = useSelector((state) => state.missions);
 
   useEffect(() => {
     if (!readyToFetch) return;
@@ -13,9 +15,25 @@ const MissionsPage = () => {
     dispatch(fetchMissions());
   }, [dispatch]);
 
+  if (isLoading) {
+    return (
+      <div className="display-when-non-list container">
+        <h1>Loading...</h1>
+      </div>
+    );
+  }
+
+  if (error) {
+    return (
+      <div className="display-when-non-list container">
+        <h1>{error}</h1>
+      </div>
+    );
+  }
+
   return (
-    <div>
-      <h1>Missions</h1>
+    <div className="container">
+      <MissionsList missions={missions} />
     </div>
   );
 };
