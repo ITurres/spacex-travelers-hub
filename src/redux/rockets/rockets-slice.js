2f2bdd9a621a9cd71f2d89910d123b1a5beb5e5c
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-07-25 06:34:38 +0300

    Added the rocket redux slice

--- src/redux/rockets/rockets-slice.js
+++ src/redux/rockets/rockets-slice.js
@@ -1,1 +1,24 @@
-// rockets component
+// rocket-slice.js
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+export const fetchRockets = createAsyncThunk('rocket/fetchRockets', async () => {
+  const response = await axios.get('https://api.spacexdata.com/v3/rockets');
+  return response.data.map(({
+    id, name, type, flickrImages,
+  }) => ({
+    id, name, type, flickrImages,
+  }));
+});
+
+export const rocketSlice = createSlice({
+  name: 'rocket',
+  initialState: { rockets: [] },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchRockets.fulfilled, (state, action) => ({ ...state, rockets: action.payload }));
+  },
+});
+
+export default rocketSlice.reducer;
