f854af8bfee5f88c469d7f2f08cd88e20222d2d8
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-07-25 23:58:08 +0300

    Added cancel reservation for the rockets from the profile page

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,15 +1,25 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { Row, Col, ListGroup } from 'react-bootstrap';
-import { filterReservedRockets } from '../redux/rockets/rockets-slice';
+import {
+  Row, Col, ListGroup, Button,
+} from 'react-bootstrap';
+import {
+  cancelRocket,
+  filterReservedRockets,
+} from '../redux/rockets/rockets-slice';
 
 const ProfilePage = () => {
-  const { filteredRockets } = useSelector((state) => state.rockets);
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(filterReservedRockets());
   }, [dispatch]);
+
+  const handleCancelRocket = (id) => {
+    dispatch(cancelRocket(id));
+  };
+
+  const { filteredRockets } = useSelector((state) => state.rockets);
 
   return (
     <Row>
@@ -23,6 +33,13 @@ const ProfilePage = () => {
                 className="d-flex justify-content-between align-items-center"
               >
                 <span>{name}</span>
+                <Button
+                  variant="outline-danger"
+                  onClick={() => handleCancelRocket(id)}
+                  className="ms-3"
+                >
+                  Cancel Reservation
+                </Button>
               </ListGroup.Item>
             ))
           ) : (
