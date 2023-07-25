f4b4c7ec78938b49b72fe884238939e9e48de98b
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-07-25 06:18:44 +0300

    Added the basic setup for Redux store

--- src/redux/config-store.js
+++ src/redux/config-store.js
@@ -1,1 +1,10 @@
-// config store
+import { configureStore } from '@reduxjs/toolkit';
+import rocketsReducer from './rockets/rockets-slice';
+
+const store = configureStore({
+  reducer: {
+    rockets: rocketsReducer,
+  },
+});
+
+export default store;
