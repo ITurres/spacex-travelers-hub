535870c5e9a3fe5992ce579c07b089cf4d476ed6
iturres <emanueliturres.co@gmail.com>
2023-07-25 15:59:17 -0300

    feat: on 'MissionsPage' > dispatch missions data fetcher

--- src/pages/MissionsPage.jsx
+++ src/pages/MissionsPage.jsx
@@ -1,6 +1,18 @@
-const MissionsPage = () => (
-  <div>
-    <h1>Missions</h1>
-  </div>
-);
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { fetchMissions } from '../redux/missions/missions-slice';
+
+const MissionsPage = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchMissions());
+  }, [dispatch]);
+
+  return (
+    <div>
+      <h1>Missions</h1>
+    </div>
+  );
+};
 export default MissionsPage;
