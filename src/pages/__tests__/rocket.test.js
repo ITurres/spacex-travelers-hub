a5fa55c485663b459a2a4596db2e3172ae5fc79e
iturres <emanueliturres.co@gmail.com>
2023-07-26 20:00:02 -0300

    fix: on 'rocket.test.js' format code to remove ESLint errors

--- src/pages/__tests__/rocket.test.js
+++ src/pages/__tests__/rocket.test.js
@@ -12,20 +12,24 @@ describe('RocketsPage component', () => {
         id: 1,
         name: 'Falcon 1',
         description: 'Small launch vehicle',
-        flickrImages: ['https://farm1.staticflickr.com/578/23451156376_8983a8ebc7_b.jpg'],
+        flickrImages: [
+          'https://farm1.staticflickr.com/578/23451156376_8983a8ebc7_b.jpg',
+        ],
       },
       {
         id: 2,
         name: 'Falcon 9',
         description: 'Medium lift launch vehicle',
-        flickrImages: ['https://farm1.staticflickr.com/330/18848515590_6a5727a479_b.jpg'],
+        flickrImages: [
+          'https://farm1.staticflickr.com/330/18848515590_6a5727a479_b.jpg',
+        ],
       },
     ];
 
     const { getByText } = render(
       <Provider store={store}>
         <RocketsPage state={rockets} />
-      </Provider>
+      </Provider>,
     );
 
     expect(getByText('Falcon 1')).toBeInTheDocument();
