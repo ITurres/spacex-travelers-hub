95c51fa51d9ab228092671e6773dd1ced4184130
iturres <emanueliturres.co@gmail.com>
2023-07-26 20:26:23 -0300

    fix: on 'rocket-card.test.js' Format code to remove ESLint errors

--- src/components/__tests__/rocket-card.test.js
+++ src/components/__tests__/rocket-card.test.js
@@ -1,18 +1,18 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import { Provider } from "react-redux"; // import Provider from react-redux
-import RocketCard from "../rockets/RocketCard";
-import store from "../../redux/config-store";
+import React from 'react';
+import { render } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import RocketCard from '../rockets/RocketCard';
+import store from '../../redux/config-store';
 
-describe("RocketCard", () => {
-  it("renders without errors", () => {
+describe('RocketCard', () => {
+  it('renders without errors', () => {
     const props = {
       id: 1,
-      name: "Falcon 1",
+      name: 'Falcon 1',
       description:
-        "The Falcon 1 was an expendable launch system privately developed and manufactured by SpaceX during 2006-2009.",
+        'The Falcon 1 was an expendable launch system privately developed and manufactured by SpaceX during 2006-2009.',
       flickrImages: [
-        "https://farm1.staticflickr.com/929/28787338307_3453a11a77_b.jpg",
+        'https://farm1.staticflickr.com/929/28787338307_3453a11a77_b.jpg',
       ],
       reserved: false,
     };
@@ -20,7 +20,7 @@ describe("RocketCard", () => {
     render(
       <Provider store={store}>
         <RocketCard rocketProps={props} />
-      </Provider>
+      </Provider>,
     );
   });
 });
